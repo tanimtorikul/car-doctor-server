f88a1afc089c9e7eac05da2f14e2a1dd5ef61ea2
tanimtorikul <torikulswe@gmail.com>
2023-11-02 13:58:08 +0600

    added jwt in server side

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
@@ -7,8 +9,14 @@ const port = process.env.PORT || 5000;
 
 // middlewares
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 // console.log(process.env.DB_PASS);
 
@@ -31,6 +39,23 @@ async function run() {
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
 
+    // auth relared apis
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    // services related apis
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -50,6 +75,7 @@ async function run() {
     // bookings
     app.get("/bookings", async (req, res) => {
       // console.log(req.query.email);
+      console.log("token", req.cookies.token);
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
